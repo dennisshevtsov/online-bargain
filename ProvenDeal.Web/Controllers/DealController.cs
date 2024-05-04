efc18aeede9435c477058c0ac937e0a482c5c936
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-05-04 08:40:36 -0700

    add deal controller

---
+++ ProvenDeal.Web/Controllers/DealController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProvenDeal.Web.Controllers;
+
+public sealed class DealController : Controller
+{
+  public IActionResult Get()  => Ok();
+  public IActionResult Post() => Ok();
+}
---
+++ ProvenDeal.Web/Dtos/DealDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ProvenDeal.Web.Dtos;
+
+public sealed class DealDto
+{
+  public required string Description { get; set; }
+
+  public required string Customer { get; set; }
+
+  public required string Executor { get; set; }
+
+  public required decimal Sum { get; set; }
+}
