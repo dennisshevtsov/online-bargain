80a99db814514fe7322991cfbf2b87a1aead9593
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-05-05 09:47:07 -0700

    add deal

---
+++ src/ProvenDeal/DealEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvenDeal;
+
+public sealed class DealEntity
+{
+  public required Guid DealId { get; set; }
+
+  public required string Description { get; set; }
+}
